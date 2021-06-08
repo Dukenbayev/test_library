4599fed83e63e90b0825c546849aacc51bf7da60
Dukenbayev <marlen.dukenbaev@gmail.com>
2021-06-08 13:23:32 +0600

    use the function to change the case

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/Dukenbayev/execute_library"
-	"strings"
 )
 
 func main()  {
 
 	fmt.Println(execute_library.Sum(1,2))
-	fmt.Println( strings.ToUpper("teseESt"))
+	fmt.Println(execute_library.Convert(""))
 }
 
